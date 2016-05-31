2694c8867d4d320588a4fe2a48f1ca4105ba186e
Michal Halaj <michal.halaj@mautilus.com>
2016-05-31 13:06:02 +0200

    - changed player script. There was mistake in moving player using AddForce method. Player kept moving also in case that no key was pressed. Now the velocity value is set according to the pressed key and the current speed.

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+    private Rigidbody2D rb;
+
+    public Vector2 speed = new Vector2(10, 10);
+
+    private Vector2 movement;
+	// Use this for initialization
+	void Start ()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        movement = new Vector2(moveHorizontal * speed.x, moveVertical * speed.y);
+
+    }
+
+    void FixedUpdate()
+    {       
+        rb.velocity = movement;
+    }
+}

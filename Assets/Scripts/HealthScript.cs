1e66c54f54653dfdb8c66bea50df3559629d2214
Michal Halaj <michal.halaj@mautilus.com>
2016-05-31 14:16:08 +0200

    - added HealthScript.cs that will destroy the attached object if it was hit by shot. - added ShotScript that contains properties of the shot like value of damage, if this is shot shoot by enemy or not.

---
+++ Assets/Scripts/HealthScript.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class HealthScript : MonoBehaviour
+{
+    public int hp = 1;
+
+    public bool isEnemy = false;
+
+    public void Damage(int damage)
+    {
+        if(damage < 0)
+        {
+            return;
+        }
+        
+        hp -= damage;
+
+        if(hp <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+	// Use this for initialization
+	void Start ()
+    {
+	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+
+    void OnTriggerEnter2D(Collider2D otherCollider)
+    {
+        ShotScript shot = otherCollider.gameObject.GetComponent<ShotScript>();
+
+        if(shot != null)
+        {
+            if(shot.isEnemyShot != isEnemy)
+            {
+                Damage(shot.damage);
+                Destroy(shot.gameObject);
+            }            
+        }
+    }
+}
---
+++ Assets/Scripts/ShotScript.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ShotScript : MonoBehaviour
+{
+    public bool isEnemyShot = false;
+
+    public int damage = 1;
+
+	// Use this for initialization
+	void Start ()
+    {
+        Destroy(gameObject, 20);
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+	
+	}
+}

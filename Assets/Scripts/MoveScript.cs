87122ff7ceac583fd81a0cc7f85b60b9b04ce6f1
Michal Halaj <michal.halaj@mautilus.com>
2016-05-31 13:17:23 +0200

    - added script that is responsible for enemy movement. refactoring of the player script - some changes in names of the private variables.

--- Assets/Scripts/MoveScript.cs
+++ Assets/Scripts/MoveScript.cs
@@ -1,16 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class PlayerScript : MonoBehaviour
+public class MoveScript : MonoBehaviour
 {
     public Vector2 speed = new Vector2(10, 10);
 
+    public Vector2 direction = new Vector2(-1, 0);
+
     private Vector2 _movement;
-    private Rigidbody2D _rigidbody;
-    // Use this for initialization
-    void Start ()
+
+    private Rigidbody2D _rigidBody = null;
+
+	// Use this for initialization
+	void Start ()
     {
-        _rigidbody = GetComponent<Rigidbody2D>();
+        _rigidBody = GetComponent<Rigidbody2D>();
     }
 	
 	// Update is called once per frame
@@ -19,12 +23,11 @@ public class PlayerScript : MonoBehaviour
         float moveHorizontal = Input.GetAxis("Horizontal");
         float moveVertical = Input.GetAxis("Vertical");
 
-        _movement = new Vector2(moveHorizontal * speed.x, moveVertical * speed.y);
-
-    }
+        _movement = new Vector2(moveHorizontal, moveVertical);
+	}
 
     void FixedUpdate()
     {
-        _rigidbody.velocity = _movement;
+        _rigidBody.velocity = _movement;
     }
 }

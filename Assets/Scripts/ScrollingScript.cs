00e541cdc9335a729a76c8100c1cc70518351b7a
Michal Halaj <michal.halaj@mautilus.com>
2016-06-01 11:26:29 +0200

    - ScrollingScript used to move background and platforms. Created simple paralax effect. Not completed yet.

---
+++ Assets/Scripts/ScrollingScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScrollingScript : MonoBehaviour {
+
+    public Vector2 direction = new Vector2(-1, 0);
+
+    public Vector2 speed = new Vector2(2, 2);
+
+    public bool isLinkedToCamera = false;
+
+    private Rigidbody2D _rigidBody = null;
+
+    private Vector2 _movement;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _rigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _movement = new Vector2(direction.x * speed.x, direction.y * speed.y);
+    }
+
+    void FixedUpdate()
+    {
+        _rigidBody.velocity = _movement;
+    }
+}
